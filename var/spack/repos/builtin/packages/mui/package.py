1b2f6b788ff1b96ceea54768d349bd126c7dd44f
Chris Richardson <chris@bpi.cam.ac.uk>
2021-10-01 11:01:36 +0100

    Add FORTRAN and RBF support

--- var/spack/repos/builtin/packages/mui/package.py
+++ var/spack/repos/builtin/packages/mui/package.py
@@ -17,13 +17,20 @@ class Mui(CMakePackage):
     version('master', branch='master')
 
     variant('cwrapper', default=False, description='C wrappers')
+    variant('fwrapper', default=False, description='FORTRAN wrappers')
+    variant('rbf', default=False, description='Radial basis function support')
 
     depends_on("cmake@3.18:", type="build")
+    depends_on("eigen@3.3:", type=("build", "run"), when="+rbf")
 
     def cmake_args(self):
         args = [
             "-DC_WRAPPER=%s" % (
                 'ON' if "+cwrapper" in self.spec else 'OFF'),
+            "-DFORTRAN_WRAPPER=%s" % (
+                'ON' if "+fwrapper" in self.spec else 'OFF'),
+            "-DUSE_RBF=%s" % (
+                'ON' if "+rbf" in self.spec else 'OFF'),
         ]
         return args
 
